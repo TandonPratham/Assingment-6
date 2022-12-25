8ae05af6e258d16b4af985bde4f598b9d74bad37
TandonPratham <117387692+TandonPratham@users.noreply.github.com>
2022-12-25 17:50:08 +0530

    Update employee.py

--- employee.py
+++ employee.py
@@ -1,10 +1,5 @@
 import json
 
-    
-# with open(r'C:\Users\ASUS\Desktop\Assigment\Assigment\_6_Assingment_6.py\_1_\employee.json','r') as file:
-#     data = json.load(file)
-#     print(data)
-
 class Employees:
     def __init__(self,name,dob,height,city,state):
         self.name = name
@@ -13,10 +8,13 @@ class Employees:
         self.city = city
         self.state = state
 
-with open('employee.json','r') as f :
-    data = json.load(f)
-
+# Open a json file
+with open('employee.json','r') as file :
+    data = json.load(file)
+    
+# Create a list of Employee object from the dict
 employees = [Employees(e['Name'], e['DOB'], e['Height'], e['City'] , e['State']) for e in data['Employees']]
 
+# Printing the list
 for employee in employees:
     print(employee.name, employee.dob, employee.height, employee.city, employee.state)
